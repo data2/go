bd617b4e5b93dafaeba196d60dd3300841d57ac6
data2 <muskteerathos@gmail.com>
2020-07-15 14:46:33 +0800

    接受入参

--- webapp.go
+++ webapp.go
@@ -7,7 +7,14 @@ import (
 
 func webfun(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Fprint(w, "开启go web")
+	fmt.Fprintln(w, "开启go web")
+	for k, v := range r.URL.Query() {
+		fmt.Println("key:", k, ", value:", v[0])
+	}
+	for k, v := range r.PostForm {
+		fmt.Println("key:", k, ", value:", v[0])
+	}
+
 }
 
 func main() {
